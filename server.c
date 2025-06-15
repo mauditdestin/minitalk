45720d2c4accf1b38170b3fbd5fdb843e52eca2e
catlover4242 <akapaui@protonmail.com>
2025-06-15 14:52:18 +0200

    + | Added ASCII Art to minitalk

--- server.c
+++ server.c
@@ -18,19 +18,35 @@ static void handler(int signum)
 	
 }
 
+void	print_ascii_header(void)
+{
+	ft_printf(
+		"   _____     ______     _____     __      __    ______     _____                    _____      _____     _____  \n"
+		"  / ____|   |  ____|   |  __ \\    \\ \\    / /   |  ____|   |  __ \\                  |  __ \\    |_   _|   |  __ \\ \n"
+		" | (___     | |__      | |__) |    \\ \\  / /    | |__      | |__) |                 | |__) |     | |     | |  | |\n"
+		"  \\___ \\    |  __|     |  _  /      \\ \\/ /     |  __|     |  _  /                  |  ___/      | |     | |  | |\n"
+		"  ____) |   | |____    | | \\ \\       \\  /      | |____    | | \\ \\                  | |         _| |_    | |__| |\n"
+		" |_____/    |______|   |_|  \\_\\       \\/       |______|   |_|  \\_\\                 |_|        |_____|   |_____/ \n"
+		"                                                                                                                \n"
+		"                                                                                                                \n"
+	);
+}
+
 int	main(int argc, char **argv)
 {
 	int	pid;
-	
+
 	(void)argv;
 	if (argc != 1)
 	{
 		ft_printf("Correct syntax : ./server\n");
+		return (0);
 	}
 	pid = getpid();
-	ft_printf("Server PID : %d\n", pid);
+	print_ascii_header();
+	ft_printf("🚀 Server PID : %d 🚀\n\n", pid);
 	signal(SIGUSR1, handler);
-	signal(SIGURS2, handler);
+	signal(SIGUSR2, handler);
 	while (42)
 		pause();
 	return (0);
